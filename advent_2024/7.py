48caddf7d11b0414b9896ec09ab798c06cc74584
Omar Ahmad <152654764+real0mar@users.noreply.github.com>
2024-12-10 17:14:06 -0700

    7 works, going to try performance optimization

--- advent_2024/7.py
+++ advent_2024/7.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+from time import time
 
 EXAMPLE = """190: 10 19
 3267: 81 40 27
@@ -14,26 +15,13 @@ REAL_IN = Path("advent_2024/7_input.txt").read_text()
 
 
 def evaluate_truth(expression: str, concat=False) -> int:
-    """Evaluates whether a given expression can achieve a target value using a combination of addition and multiplication.
-    The expression is a string formatted as "target:n1 n2 n3 ...", where "target" is the integer value to achieve,
-    and "n1 n2 n3 ..." are the integers that can be used in the operations.
-    The function uses a helper function to recursively check if the target can be achieved by either adding or multiplying
-    the numbers in the given order.
-
-    Args:
-        expression (str): The input string containing the target and the list of numbers.
-
-    Returns:
-        int: The target value if it can be achieved, otherwise 0.
-
-    """
     if not expression:
         return 0
     split = expression.split(":")
     target = int(split[0])
     numbers = list(map(int, split[1].split()))
 
-    def helper(index, current_value):
+    def helper(index: int, current_value: int) -> bool:
         if index == len(numbers):
             return current_value == target
 
@@ -41,11 +29,10 @@ def evaluate_truth(expression: str, concat=False) -> int:
             return helper(index + 1, current_value + numbers[index]) or helper(
                 index + 1, current_value * numbers[index]
             )
-
-        return helper(index + 1, current_value * numbers[index]) or helper(
-            index + 1,
-            current_value + numbers[index]
-            or helper(index + 1, int(str(current_value) + str(numbers[index]))),
+        return (
+            helper(index + 1, current_value * numbers[index])
+            or helper(index + 1, current_value + numbers[index])
+            or helper(index + 1, int(str(current_value) + str(numbers[index])))
         )
 
     if helper(1, numbers[0]):
@@ -54,18 +41,20 @@ def evaluate_truth(expression: str, concat=False) -> int:
 
 
 def part1(input: str) -> int:
-    return sum(evaluate_truth(expression) for expression in input.split("\n"))
+    return sum(evaluate_truth(expression) for expression in input.splitlines())
 
 
 def part2(input: str) -> int:
     return sum(
-        evaluate_truth(expression, concat=True) for expression in input.split("\n")
+        evaluate_truth(expression, concat=True) for expression in input.splitlines()
     )
 
 
 if __name__ == "__main__":
+    start = time()
     assert part1(EXAMPLE) == 3749
     assert part2(EXAMPLE) == 11387
 
     print(part1(REAL_IN))
     print(part2(REAL_IN))
+    print(f"Time: {time() - start}")
